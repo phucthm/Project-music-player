95434bb636fecbaaae7db29fb91a3062b3b43320
phuc Tran <20127285@student.hcmus.edu.vn>
2022-07-14 11:21:00 +0700

    First demo front end

--- back-end/controllers/goalController.js
+++ back-end/controllers/goalController.js
@@ -1,23 +1,28 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../model/goalModle')
+const User = require('../model/userModel')
+
 
 const getGoals = asyncHandler( async(req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
 
     res.status(200).json(goals)
 })
 
 const setGoals = asyncHandler(async(req, res) => {
+
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text')
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: user.id
     })
     res.status(200).json(goal)
+
 })
 
 const putGoals = asyncHandler(async(req, res) => {
@@ -26,6 +31,20 @@ const putGoals = asyncHandler(async(req, res) => {
     if(!goal){
         res.status(400)
         throw Error('Goal not found')
+    }
+ 
+    const user = await User.findById(req.user.id)
+
+    // Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged user matches the user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{
@@ -41,6 +60,18 @@ const deleteGoals = asyncHandler(async(req, res) => {
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    // Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged user matches the user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     await goal.remove()
--- back-end/routes/goalRoutes.js
+++ back-end/routes/goalRoutes.js
@@ -3,8 +3,10 @@ const express = require('express')
 const router  = express.Router()
 const {getGoals, setGoals, putGoals, deleteGoals} = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoals)
-router.route('/:id').put(putGoals).delete(deleteGoals)
+const {protect} = require('../Errors/authErrors')
+
+router.route('/').get(protect, getGoals).post(protect, setGoals)
+router.route('/:id').put(protect, putGoals).delete(protect, deleteGoals)
 
 
 module.exports = router
