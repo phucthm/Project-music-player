0881860df44e27e5ab7353d1f6edb0a95f6aab3b
phuc Tran <20127285@student.hcmus.edu.vn>
2022-07-15 20:05:51 +0700

    fix back-end

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const conneectDB = async () => {
+const connectDB = async () => {
     try {
         const conn = await mongoose.connect('mongodb+srv://phucthm:phucthm@phuc.emwohvw.mongodb.net/?retryWrites=true&w=majority')
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
@@ -10,4 +10,4 @@ const conneectDB = async () => {
     }
 }
 
-module.exports = conneectDB
+module.exports = connectDB
